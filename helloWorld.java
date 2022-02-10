62645843f979c848c53109615eee8914901ea0c9
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2022-02-09 21:47:50 -0500

    add helloWorld.java to main branch...

---
+++ helloWorld.java
@@ -0,0 +1,6 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.print("Who are you? My friend...\n");
+        System.out.print("Hello, i am xxr!\n");
+    }
+}
