85587ad6f710b83c10771f5e0a6b77ad1102e9a4
yunji0387 <yunji0387@gmail.com>
2022-02-09 20:55:57 -0600

    YunJi edit

--- helloWorld.java
+++ helloWorld.java
@@ -2,5 +2,6 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.print("Who are you? My friend...\n");
         System.out.print("Hello, i am xxr!\n");
+        System.out.println("Hello , Im YunJi");
     }
 }
